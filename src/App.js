31c876b2cd397a32ff6ab111f7e596f8eefa0fe9
speedo1004 <speedodeveloper1004@gmail.com>
2023-01-31 15:02:35 +0900

    add assets

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header"; //Include Header
+import Footer from "./Footer";
+import SingleProduct from "./SingleProduct";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <SingleProduct></SingleProduct>
+      <Footer></Footer>
     </div>
   );
 }
-
 export default App;
